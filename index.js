0669c328cae053eb4da8d5098ac15cac122c8828
colorMeCode <good-vibrations@hotmail.com>
2023-09-26 17:31:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { execSync } = require("child_process");
 const readFileSync = require("fs").readFileSync;
 
 async function run() {
+  let pr1Branch;
+
   try {
     const token = core.getInput("github-token", { required: true });
     const octokit = github.getOctokit(token);
@@ -16,13 +18,18 @@ async function run() {
       (pr) => pr.number !== pullRequest.number
     );
 
+    pr1Branch = await getBranchName(octokit, repo, pullRequest.number);
+    const pr1Files = await getChangedFiles(octokit, repo, pr1Number);
+
+    prefetchBranches(pr1Branch);
+
     let conflictArray = [];
 
     for (const openPullRequest of otherOpenPullRequests) {
       const conflictData = await checkForConflicts({
         octokit,
         repo,
-        pr1Number: pullRequest.number,
+        pr1Files,
         pr2Number: openPullRequest.number,
       });
 
@@ -58,6 +65,41 @@ async function run() {
     }
   } catch (error) {
     core.setFailed(error.message);
+  } finally {
+    cleanup(pr1Branch);
+  }
+}
+
+function prefetchBranches(pr1Branch) {
+  if (!pr1Branch) {
+    throw new Error("Failed to fetch branch name for main PR");
+  }
+
+  try {
+    // Configure Git with a dummy user identity
+    execSync(`git config user.email "action@github.com"`);
+    execSync(`git config user.name "GitHub Action"`);
+
+    execSync(`git fetch origin main:main`);
+
+    // Merge main into PR1 in memory
+    execSync(`git checkout refs/remotes/origin/tmp_${pr1Branch}`);
+    execSync(`git merge main --no-commit --no-ff`);
+
+    execSync(
+      `git fetch origin ${pr1Branch}:refs/remotes/origin/tmp_${pr1Branch}`
+    );
+  } catch (error) {
+    console.error(`Error during prefetch process: ${error.message}`);
+  }
+}
+
+function cleanup(pr1Branch) {
+  try {
+    // Cleanup by deleting temporary ref
+    execSync(`git update-ref -d refs/remotes/origin/tmp_${pr1Branch}`);
+  } catch (error) {
+    console.error(`Error during cleanup process: ${error.message}`);
   }
 }
 
@@ -83,15 +125,8 @@ async function getOpenPullRequests(octokit, repo) {
   }
 }
 
-async function checkForConflicts({ octokit, repo, pr1Number, pr2Number }) {
-  const pr1Branch = await getBranchName(octokit, repo, pr1Number);
+async function checkForConflicts({ octokit, repo, pr1Files, pr2Number }) {
   const pr2Branch = await getBranchName(octokit, repo, pr2Number);
-
-  if (!pr1Branch || !pr2Branch) {
-    throw new Error("Failed to fetch branch name for one or both PRs.");
-  }
-
-  const pr1Files = await getChangedFiles(octokit, repo, pr1Number);
   const pr2Files = await getChangedFiles(octokit, repo, pr2Number);
 
   const overlappingFiles = pr1Files.filter((file) => pr2Files.includes(file));
@@ -100,7 +135,7 @@ async function checkForConflicts({ octokit, repo, pr1Number, pr2Number }) {
     return [];
   }
 
-  const conflictData = await attemptMerge(pr1Branch, pr2Branch);
+  const conflictData = await attemptMerge(pr2Branch);
 
   return conflictData;
 }
@@ -111,6 +146,12 @@ async function getBranchName(octokit, repo, prNumber) {
     repo: repo.repo,
     pull_number: prNumber,
   });
+
+  const branchName = pr.head.ref;
+
+  if (!branchName) {
+    throw new Error(`Failed to fetch branch name for ${prNumber}`);
+  }
 
   return pr.head.ref;
 }
@@ -124,12 +165,10 @@ async function getChangedFiles(octokit, repo, prNumber) {
 
   return files.map((file) => file.filename);
 }
+
 function extractConflictingLineNumbers(filePath) {
   const fileContent = readFileSync(filePath, "utf8");
   const lines = fileContent.split("\n");
-
-  console.log(`Total lines processed: ${lines.length}`);
-  console.log(lines.slice(320).join("\n"));
 
   let lineCounter = 0;
   const conflictLines = [];
@@ -141,37 +180,31 @@ function extractConflictingLineNumbers(filePath) {
 
   for (const line of lines) {
     if (!inOursBlock && !inTheirsBlock) {
-      lineCounter++;  // Increment only outside of conflict blocks.
+      lineCounter++; // Increment only outside of conflict blocks.
     }
 
     if (line.startsWith("<<<<<<< HEAD")) {
       inOursBlock = true;
       conflictStartLine = lineCounter;
-      console.log(`Conflict started at line: ${conflictStartLine}`);
       continue;
     }
 
     if (line.startsWith("=======")) {
       inOursBlock = false;
       inTheirsBlock = true;
-      console.log(`Switching to THEIRS block at line: ${lineCounter}`);
       continue;
     }
 
     if (line.startsWith(">>>>>>>")) {
       inTheirsBlock = false;
-      console.log(`Conflict end detected at line: ${lineCounter}`);
-      console.log('Comparing Ours and Theirs Block');
-      console.log('Ours Block:', oursBlock);
-      console.log('Theirs Block:', theirsBlock);
 
       oursBlock.forEach((ourLine, index) => {
-        if (theirsBlock[index] !== undefined && ourLine !== theirsBlock[index]) {
-          const actualLineNumber = conflictStartLine + index; 
+        if (
+          theirsBlock[index] !== undefined &&
+          ourLine !== theirsBlock[index]
+        ) {
+          const actualLineNumber = conflictStartLine + index;
           conflictLines.push(actualLineNumber);
-          console.log(`Conflict detected at line: ${actualLineNumber}`);
-          console.log(`Ours: ${ourLine}`);
-          console.log(`Theirs: ${theirsBlock[index]}`);
         }
       });
 
@@ -190,37 +223,22 @@ function extractConflictingLineNumbers(filePath) {
   return conflictLines;
 }
 
-
-
-
-async function attemptMerge(pr1, pr2) {
+async function attemptMerge(pr2Branch) {
   const conflictData = {};
 
   try {
-    // Configure Git with a dummy user identity
-    execSync(`git config user.email "action@github.com"`);
-    execSync(`git config user.name "GitHub Action"`);
-
-    // Fetch PR branches into temporary refs
-    execSync(`git fetch origin ${pr1}:refs/remotes/origin/tmp_${pr1}`);
-    execSync(`git fetch origin ${pr2}:refs/remotes/origin/tmp_${pr2}`);
-
-    // Fetch the main branch
-    execSync(`git fetch origin main:main`);
-    
-    // Merge main into PR1 in memory
-    execSync(`git checkout refs/remotes/origin/tmp_${pr1}`);
-    execSync(`git merge main --no-commit --no-ff`);
+    // Fetch conflicting PR branch into temporary ref
+    execSync(
+      `git fetch origin ${pr2Branch}:refs/remotes/origin/tmp_${pr2Branch}`
+    );
 
     // Merge main into PR2 in memory
     execSync(`git checkout refs/remotes/origin/tmp_${pr2}`);
     execSync(`git merge main --no-commit --no-ff`);
 
-    // Attempt to merge PR2's branch into PR1 in memory without committing or fast-forwarding
-    execSync(`git checkout refs/remotes/origin/tmp_${pr1}`);
     try {
+      // Attempt to merge PR2's branch in memory without committing or fast-forwarding
       execSync(`git merge refs/remotes/origin/tmp_${pr2} --no-commit --no-ff`);
-      console.log("Merge successful");
     } catch (mergeError) {
       const stdoutStr = mergeError.stdout.toString();
       if (stdoutStr.includes("Automatic merge failed")) {
@@ -238,8 +256,7 @@ async function attemptMerge(pr1, pr2) {
     console.error(`Error during merge process: ${error.message}`);
   } finally {
     execSync(`git reset --hard HEAD`); // Reset any changes
-    // Cleanup by deleting temporary refs
-    execSync(`git update-ref -d refs/remotes/origin/tmp_${pr1}`);
+    // Cleanup by deleting temporary ref
     execSync(`git update-ref -d refs/remotes/origin/tmp_${pr2}`);
   }
 
@@ -289,7 +306,6 @@ async function createConflictComment({
     throw error;
   }
 }
-
 
 async function requestReviews({
   octokit,
