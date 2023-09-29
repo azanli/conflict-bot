2a1fe177dc02a017ae6297351b0351ba2a77d50c
Andy Lin <andyzanli@gmail.com>
2023-09-28 22:01:26 -0700

    Change comment heading

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ async function createConflictComment({
   conflictArray,
 }) {
   try {
-    let conflictMessage = "### Conflicts Found\n\n";
+    let conflictMessage = "### 🤖 Merge Issues Detected\n\n";
 
     for (const data of conflictArray) {
       conflictMessage += `<details>\n`;
